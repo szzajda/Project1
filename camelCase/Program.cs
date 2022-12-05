347061718670f8a6bfcee31350dcc35e2d2ba45f
szzajda <119533102+szzajda@users.noreply.github.com>
2022-12-05 19:12:22 +0100

    Add files via upload

---
+++ camelCase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace camelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide me with a sentence: ");
+            string phrase = Console.ReadLine();
+
+            string titleCasePhrase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
+
+            string lowerCaseFirstLetter = char.ToLower(titleCasePhrase[0]) + titleCasePhrase.Substring(1);
+
+            string noSpacesPhrase = lowerCaseFirstLetter.Replace(" ", string.Empty);
+
+            Console.WriteLine("Camel case of the sentence is: " + noSpacesPhrase);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
